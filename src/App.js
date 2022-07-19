e42fa343eb1c3e3d0fb7b5593bfa9ffc58ec2aa1
KamiSmol <65988871+KamiSmol@users.noreply.github.com>
2022-07-19 19:33:37 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,10 +22,10 @@ class App extends Component {
       <Form name="form1" layout={"horizontal"}>
         <Form.Header>黄金分割率</Form.Header>
         <Form.Item label={"请输入A点"} name={"min"}>
-          <Input placeholder={"请输入值"} clearable onChange={(min) => this.setState({ min: parseFloat(min) })} />
+          <Input placeholder={"请输入值"} inputmode={"decimal"} clearable onChange={(min) => this.setState({ min: parseFloat(min) })} />
         </Form.Item>
         <Form.Item label={"请输入B点"} name={"max"}>
-          <Input placeholder={"请输入值"} clearable onChange={(max) => this.setState({ max: parseFloat(max) })} />
+          <Input placeholder={"请输入值"} inputmode={"numeric"} clearable onChange={(max) => this.setState({ max: parseFloat(max) })} />
         </Form.Item>
       </Form>
       <Table pagination={false} columns={colunms} dataSource={[{ key: 1, SpcVal: 0.809, result: this.compute(0.809) }, { key: 2, SpcVal: 0.618, result: this.compute(0.618) }, { key: 3, SpcVal: 0.5, result: this.compute(0.5) }, { key: 4, SpcVal: 0.382, result: this.compute(0.382) }, { key: 5, SpcVal: 0.236, result: this.compute(0.236) }]} />

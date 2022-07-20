5785abef0d8f511caa517908cc4ccd09904c101a
unknown <2451473788@qq.com>
2022-07-20 08:57:42 +0800

    change

--- src/index.js
+++ src/index.js
@@ -9,13 +9,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <>
-    <SafeArea position={"top"} />
-<App />
+      <div style={{ background: '#fff' }}>
+        <SafeArea position={"top"} />
+      </div>
+      <App />
     </>
-
-
-    
-    
   </React.StrictMode>
 );
 

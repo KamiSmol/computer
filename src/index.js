8994db6290fbe4e95cf125e40420af5c6429f33d
unknown <2451473788@qq.com>
2022-07-20 11:29:12 +0800

    change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'antd/dist/antd.min.css';
-import 'antd-mobile/bundle/css-vars-patch.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 

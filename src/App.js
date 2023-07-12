176b3a18bcdd1d4dbf97a732cd5dfa87a374fe96
KamiSmol <65988871+KamiSmol@users.noreply.github.com>
2023-07-12 12:18:53 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { Input, Form,Radio,Space,DemoBlock } from "antd-mobile";
+import { Input, Form, Radio, Space } from "antd-mobile";
 import { Table } from "antd"
 import { React, Component, createRef } from "react";
 import styles from './styles.less';
+
 class App extends Component {
     state = {
-        min: "",
-        max: "",
-        flag:1
+        point: "",
+        flag: 1
     }
 
     form = createRef()
@@ -24,35 +24,35 @@ class App extends Component {
         return <div className={styles.container}>
             <Form name="form" ref={this.form} layout={"horizontal"}>
                 <Form.Header>黄金分割率</Form.Header>
-                <Form.Item label={"请输入A点"} name={"min"}>
-                    <Input placeholder={"请输入值"} inputMode={"decimal"} clearable={true} onlyShowClearWhenFocus={false} onChange={(min) => {
-                        min = min.replace(/[^\d{1,}\.\d{1,}|\d{1,}]/g, '')
-                        this.setState({ min: parseFloat(min) })
+                <Form.Item label={"请输入A点"} name={"point"}>
+                    <Input placeholder={"请输入值"} inputMode={"decimal"} clearable={true} onlyShowClearWhenFocus={false} onChange={(point) => {
+                        point = point.replace(/[^\d{1,}\.\d{1,}|\d{1,}]/g, '')
+                        this.setState({ point: parseFloat(point) })
                         this.form.current.setFieldsValue({
-                            min
+                            point
                         })
                     }} />
                 </Form.Item>
-        <Form.Item label={"请选择趋势"} name={"flag"}>
-    <Radio.Group defaultValue={1} onChange={(flag) => {
-                        this.setState({ flag:flag  })
+                <Form.Item label={"请选择趋势"} name={"flag"}>
+                    <Radio.Group defaultValue={1} onChange={(flag) => {
+                        this.setState({ flag: flag })
                     }}>
-          <Space direction='horizontal'>
-            <Radio value={1}>上升</Radio>
-            <Radio value={-1}>下降</Radio>
-          </Space>
-        </Radio.Group>
-    </Form.Item>
+                        <Space direction='horizontal'>
+                            <Radio value={1}>上升</Radio>
+                            <Radio value={-1}>下降</Radio>
+                        </Space>
+                    </Radio.Group>
+                </Form.Item>
             </Form>
-            <Table pagination={false} columns={colunms} dataSource={[{ key: 1, SpcVal: 0.809, result: this.compute(0.809) }, { key: 2, SpcVal: 0.618, result: this.compute(0.618) }, { key: 3, SpcVal: 0.5, result: this.compute(0.5) }, { key: 4, SpcVal: 0.382, result: this.compute(0.382) }, { key: 5, SpcVal: 0.236, result: this.compute(0.236) }]} />
+            <Table pagination={false} columns={colunms} dataSource={this.state.flag === 1 ? [{ key: 1, SpcVal: 0.236, result: this.compute(0.236) }, { key: 2, SpcVal: 0.382, result: this.compute(0.382) }, { key: 3, SpcVal: 0.5, result: this.compute(0.5) }, { key: 4, SpcVal: 0.618, result: this.compute(0.382) }, { key: 5, SpcVal: 0.809, result: this.compute(0.236) }] : [{ key: 1, SpcVal: 0.809, result: this.compute(0.809) }, { key: 2, SpcVal: 0.618, result: this.compute(0.618) }, { key: 3, SpcVal: 0.5, result: this.compute(0.5) }, { key: 4, SpcVal: 0.382, result: this.compute(0.382) }, { key: 5, SpcVal: 0.236, result: this.compute(0.236) }]} />
         </div>
     }
 
     compute = (num) => {
-        let { max, min,flag } = this.state
-        if (typeof min !== 'number') return "";
-        let result = (1+flag*num) * min;
-        return Math.round(result * 100) / 100;
+        let { min, flag } = this.state
+        if (typeof min !== 'number') return ""
+        let result = (1 + flag * num) * min
+        return Math.round(result * 100) / 100
     }
 
 }
